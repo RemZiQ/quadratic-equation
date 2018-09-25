6b1530e9ad94e7050db59c2a77065f6cd6fb54a3
RemZiQ <remziq1992@gmail.com>
2018-09-25 16:55:40 +0300

    Update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-module.exports = function solveEquation(equation) {
-  // your implementation
+function compareNumeric( a, b ){
+  return a - b;
 }
+
+module.exports = function solveEquation( equation ) {
+  let a, 
+    b, 
+    c, 
+    discriminant, 
+    x1, 
+    x2, 
+    equationWithoutBlanks,
+    arrayOfArguments = [],
+    solutions = []; 
+    
+    
+
+  equationWithoutBlanks = equation.replace(/\s+/g, '');
+  arrayOfArguments = equationWithoutBlanks;
+
+  arrayOfArguments = arrayOfArguments.split("*x^2");
+  a = +arrayOfArguments[0];
+
+  arrayOfArguments = arrayOfArguments[1].split("*x");
+  b = +arrayOfArguments[0];
+  c = +arrayOfArguments[1];
+
+  discriminant = Math.pow(b,2) - 4 * a * c;
+  x1 = Math.round((-b + Math.sqrt(discriminant)) / (2 * a) ); 
+  x2 = Math.round((-b - Math.sqrt(discriminant)) / (2 * a));
+  solutions.push(x1,x2);
+
+  return solutions.sort(compareNumeric);
+}
+
